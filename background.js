2158d647c5fe7512c55f16d2c4112e3116198d13
Nitheeshskumar <nitheesh.s.k1710@gmail.com>
2024-12-01 22:17:51 +0530

    initial commit

---
+++ background.js
@@ -0,0 +1,18 @@
+chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true }).catch((error) => console.error(error));
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  showSummary(activeInfo.tabId);
+});
+chrome.tabs.onUpdated.addListener(async (tabId) => {
+  showSummary(tabId);
+});
+
+async function showSummary(tabId) {
+  const tab = await chrome.tabs.get(tabId);
+
+  const injection = await chrome.scripting.executeScript({
+    target: { tabId },
+    files: ["scripts/extract-content.js"],
+  });
+  chrome.storage.session.set({ pageContent: injection[0].result });
+}
